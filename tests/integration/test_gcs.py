fe0bc30cc84524874fc3f6a06dceb9187c2b9ed0
Tarjei Husøy <git@thusoy.com>
2020-02-28 10:38:35 -0800

    Add test skeleton

---
+++ tests/integration/test_gcs.py
@@ -0,0 +1,13 @@
+import unittest.mock as mock
+
+import pytest
+from google.cloud import storage
+
+from honeyflare import process_bucket_object
+
+
+pytestmark = pytest.mark.integration
+
+
+def test_process_file():
+    pass
