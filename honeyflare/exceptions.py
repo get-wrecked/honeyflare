1283556957eed6993d2062c871e682d080b2745e
Tarjei Husøy <git@thusoy.com>
2020-03-11 16:57:04 -0700

    Retry on file locked

--- honeyflare/exceptions.py
+++ honeyflare/exceptions.py
@@ -1,9 +1,8 @@
-class FileLockedError(Exception):
-    '''The given file was already locked'''
-
-
 class RetriableError(Exception):
     '''
     This is a temporary error that should cause the invocation to exit with an
     error so that it'll be retried.
     '''
+
+class FileLockedError(RetriableError):
+    '''The given file was already locked'''
