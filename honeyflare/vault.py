fdcdc5a21905f4d6f2ca17c765aaa7e6e991c041
Tarjei Husøy <git@thusoy.com>
2025-08-25 14:56:55 +0200

    Replace hvac and add retries

--- honeyflare/vault.py
+++ honeyflare/vault.py
@@ -4,8 +4,9 @@ import tempfile
 import urllib.parse
 from pathlib import PurePosixPath
 
-import hvac
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
 
 
 def get_vault_secret(vault_url):
@@ -18,6 +19,25 @@ def get_vault_secret(vault_url):
     In addition to the `key` and `ca` query parameters you can also specify `https=false`
     to request the secret over plain http.
     """
+    adapter = HTTPAdapter(
+        max_retries=Retry(
+            total=4,
+            backoff_factor=0.1,
+            status_forcelist=[412, 429, 500, 502, 503],
+            allowed_methods=(
+                "GET",
+                "HEAD",
+                # POST is safe here because we are only using it for login
+                "POST",
+            ),
+            raise_on_status=False,
+        )
+    )
+    session = requests.Session()
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
+
+    session = requests.Session()
     parsed_url = urllib.parse.urlparse(vault_url)
     parsed_parameters = urllib.parse.parse_qs(parsed_url.query)
 
@@ -27,31 +47,37 @@ def get_vault_secret(vault_url):
         scheme = "http"
 
     b64_ca_cert = parsed_parameters.get("ca")
+    request_kwargs = {}
     if b64_ca_cert:
         ca_cert = base64.urlsafe_b64decode(b64_ca_cert[0].encode("utf-8"))
         with tempfile.NamedTemporaryFile(delete=False) as fh:
             fh.write(ca_cert)
-            os.environ["REQUESTS_CA_BUNDLE"] = fh.name
+        request_kwargs["verify"] = fh.name
 
     vault_url = "%s://%s" % (scheme, parsed_url.netloc)
     vault_role = parsed_parameters.get("role", "honeyflare")
-    client = hvac.Client(url=vault_url)
 
-    client.auth.gcp.login(role=vault_role, jwt=get_auth_jwt(vault_role))
+    login_response = session.post(
+        f"{vault_url}/v1/auth/gcp/login",
+        **request_kwargs,
+        json={
+            "role": vault_role,
+            "jwt": get_auth_jwt(vault_role),
+        },
+    )
+    login_response.raise_for_status()
+
+    session.headers["x-vault-token"] = login_response.json()["auth"]["client_token"]
 
     parsed_path = PurePosixPath(parsed_url.path)
 
-    honeycomb_key = client.secrets.kv.read_secret_version(
-        path="/".join(parsed_path.parts[2:]),
-        mount_point=parsed_path.parts[1],
+    honeycomb_key = session.get(
+        f"{vault_url}/v1/{parsed_path.parts[1]}/data/{'/'.join(parsed_path.parts[2:])}"
     )
+    honeycomb_key.raise_for_status()
     key = parsed_parameters["key"][0]
 
-    if b64_ca_cert:
-        # Clean up to not affect other https queries
-        del os.environ["REQUESTS_CA_BUNDLE"]
-
-    return honeycomb_key["data"]["data"][key]
+    return honeycomb_key.json()["data"]["data"][key]
 
 
 def get_auth_jwt(vault_role):
